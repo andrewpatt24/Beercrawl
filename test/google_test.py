3b2504fdd6617fb693b1db79842821a43826f514
Andrew Patterson <andrewpatterson@ionas-mac-mini.home>
2016-06-29 22:44:52 +0100

    changing place optimsation behaviour

--- test/google_test.py
+++ test/google_test.py
@@ -2,66 +2,6 @@ from utils import get_api_key, haversine, get_centre_lat_long
 from utils import get_lat_lng, distance_array, calculate_distances
 import googlemaps
 
-class BeerCrawler:
-    """BeerCrawler class to get a beer crawl direction set"""
-    def __init__(self,api_key):
-    	self.start_string = 'The Anchor, 34 Park St, Southwark, London SE1 9EF'
-		self.end_string = 'Southwark Brewing Company'
-		self.n_places = 2
-		self.radius_factor = 1
-		self.keywords = ['bar','beer']
-		self.mode="bicycling"
-		self.api_key = api_key
-		self.gmaps = googlemaps.Client(key=api_key)
-
-
-    def get_lat_lngs(self):
-    	self.start = get_lat_lng(self.gmaps,self.start_string)
-    	self.end = get_lat_lng(self.gmaps,self.end_string)
-
-    def find_centre(self):
-    	self.centre = get_centre_lat_long(self.start,self.end)
-
-    def calculate_radius(self):
-    	self.radius = haversine(
-    		self.c_cen['lat'],
-    		self.c_cen['lng'],
-    		self.start['lat'],
-    		self.start['lng']
-    		)*1000
-
-    def get_nearby_places(self):
-    	##Get nearby places
-		self.nearby = self.gmaps.places_radar(
-			(self.c_cen['lat'], self.c_cen['lng']),
-			radius=self.radius*self.radius_factor,
-			keyword=self.keywords
-			)
-
-		self.place_ids = map(lambda x: x['place_id'],self.nearby['results'])
-		self.place_info = map(lambda x: self.gmaps.place(x),self.place_ids)
-		self.place_names = map(lambda x: x['result']['formatted_address'],self.place_info)
-
-	def calculate_distances(self):
-		self.distances = calculate_distances(
-			self.gmaps,
-			self.start,
-			self.end,
-			self.place_names,
-			self.n_places
-			)
-
-	
-
-
-
-
-
-
-
-
-
-
 
 def main(api_key):
 	# Set up gmaps object with API key
@@ -105,23 +45,23 @@ def main(api_key):
 	place_names = map(lambda x: x['result']['formatted_address'],place_info)
 
 	#calculate distances from combinations
-	distances = calculate_distances(gmaps,start,end,place_names,n_places)
+	#distances = calculate_distances(gmaps,start,end,place_names,n_places)
 
 	#get min distance for waypoints
-	min_dist = min([x[1] for x in distances])
-	print min_dist
-	final_waypoints = [x for x in distances if x[1]==min_dist]
-	print final_waypoints
-	print min_dist
+	#min_dist = min([x[1] for x in distances])
+	#print min_dist
+	#final_waypoints = [x for x in distances if x[1]==min_dist]
+	#print final_waypoints
+	#print min_dist
 
-	final_directions = gmaps.directions(
-			start,
-        	end,
-        	mode=mode,
-        	waypoints=final_waypoints[0][0]
-        	)
+	#final_directions = gmaps.directions(
+			#start,
+        	#end,
+        	#mode=mode,
+        	#waypoints=final_waypoints[0][0]
+        	#)
 
-	print final_directions
+	#print final_directions
 
 
 
